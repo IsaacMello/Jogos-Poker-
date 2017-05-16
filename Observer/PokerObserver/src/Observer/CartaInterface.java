4356d703e68e2f8b7fea25b4c575361c75e63ac8
IsaacMello <ifms@ic.ufal.br>
2017-05-15 21:35:47 -0300

    Add files via upload

---
+++ Observer/PokerObserver/src/Observer/CartaInterface.java
@@ -0,0 +1,14 @@
+package Observer;
+
+public interface CartaInterface {
+	
+	public void Ouro();
+	public void Paus();
+	public void Copas();
+	public void Espadas();
+	
+	
+	
+	}
+
+
---
+++ Observer/PokerObserver/src/Observer/Dado.java
@@ -0,0 +1,70 @@
+package Observer;
+
+import Observer.Carta;
+
+public class Dado {
+
+    private static String[] VALORES;
+    private static String[] NAIPES;
+    private Carta[] baralho;
+    private int tamanhorealbaralho;
+
+    Dado() {
+        VALORES = new String[12];
+        VALORES[0] = "2";
+        VALORES[1] = "3";
+        VALORES[2] = "4";
+        VALORES[3] = "5";
+        VALORES[4] = "6";
+        VALORES[5] = "7";
+        VALORES[6] = "8";
+        VALORES[7] = "9";
+        VALORES[8] = "J";
+        VALORES[9] = "Q";
+        VALORES[10] = "K";
+        VALORES[11] = "A";
+
+        NAIPES = new String[4];
+        NAIPES[0] = "Espadas";
+        NAIPES[1] = "Paus";
+        NAIPES[2] = "Copas";
+        NAIPES[3] = "Ouro";
+
+        baralho = new Carta[48];
+        tamanhorealbaralho = 0;
+        
+    }
+
+    public String getNaipe(int i) {
+        return this.NAIPES[i];
+    }
+
+    public String getValor(int i) {
+        return this.VALORES[i];
+    }
+
+    public void setBaralho(Carta c, int posicao) {
+        this.baralho[posicao] = c;
+    }
+
+    public Carta getBaralho(int posicao) {
+        return this.baralho[posicao];
+    }
+
+    public void setTamanhoRealBaralho(int tamanhorealbaralho) {
+        this.tamanhorealbaralho = tamanhorealbaralho;
+    }
+
+    public int getTamanhoRealBaralho() {
+        return this.tamanhorealbaralho;
+    }
+
+    public boolean testaCartaDiferente(Carta carta) {
+        for (int i = 0; i <= this.tamanhorealbaralho; i++) {
+            if (carta.equals(this.baralho[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
